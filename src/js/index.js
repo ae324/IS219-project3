7889de6d95a90be766b18a2c65162ecd9d7cd76c
allis <ae324@njit.edu>
2020-05-04 12:09:01 -0400

    assignment 3

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,17 @@
-let mountains =[
-{ name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
-{ name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
-{ name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
-{ name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
-{ name: "Monte Amiata", height: 1738, place: "Siena" }
-];
+(function ($) {
+    $('#citiesTable').DataTable( {
+        "ajax": "http://localhost:9080/api/v1/cities",
+        "columns": [
+            { "data": "id" },
+            { "data": "fldName" },
+            { "data": "fldLat" },
+            { "data": "fldLong" },
+            { "data": "fldCountry" },
+            { "data": "fldAbbreviation" },
+            { "data": "fldCapitalStatus" },
+            { "data": "fldPopulation" }
 
-function generateTableHead(table, data) {
-    let thead = table.createTHead();
-    let row = thead.insertRow();
-    for (let key of data) {
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
-}
+        ]
+    } );
 
-function generateTable(table, data) {
-    for (let element of data) {
-        let row = table.insertRow();
-        for (key in element) {
-            let cell = row.insertCell();
-            let text = document.createTextNode(element[key]);
-            cell.appendChild(text);
-        }
-    }
-}
-
-let table = document.querySelector("table");
-let data = Object.keys(mountains[0]);
-generateTableHead(table, data);
-generateTable(table, mountains);
+})(jQuery);
